756d36ecf80c7fad11db07f72cf3ee97b21e3662
ASTERISK_PC\heroe <heroes51635@mail.ru>
2021-10-12 10:45:57 +0300

    Add Model

---
+++ FlowersShop/FlowersShop/Models/Flower.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mime;
+using System.Threading.Tasks;
+
+namespace FlowersShop.Models
+{
+    public class Flower
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string SmallImageLink { get; set; }
+        public string BigImageLink { get; set; }
+        public string SmallInfo { get; set; }
+        public string BigInfo { get; set; }
+
+    }
+}
