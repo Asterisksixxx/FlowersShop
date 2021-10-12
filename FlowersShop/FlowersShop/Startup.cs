e0c54970ed3467cfda4486e3ffa44190b3b07d7b
ASTERISK_PC\heroe <heroes51635@mail.ru>
2021-10-12 12:04:54 +0300

    Last edit

--- FlowersShop/FlowersShop/Startup.cs
+++ FlowersShop/FlowersShop/Startup.cs
@@ -8,6 +8,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FlowersShop.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace FlowersShop
 {
@@ -23,7 +25,8 @@ namespace FlowersShop
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            
+            services.AddDbContext<AppDataContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllersWithViews();
         }
 
