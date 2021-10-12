820dd2571c926613bd692560a8db8502926f3a40
ASTERISK_PC\heroe <heroes51635@mail.ru>
2021-10-12 12:03:15 +0300

    Create new Model, Create model entity

--- FlowersShop/FlowersShop/Data/AppDataContext.cs
+++ FlowersShop/FlowersShop/Data/AppDataContext.cs
@@ -2,10 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FlowersShop.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace FlowersShop.Data
 {
-    public class AppDataContext
+    public class AppDataContext:DbContext
     {
+        public AppDataContext(DbContextOptions options) : base(options)
+        {
+           
+        }
+        public DbSet<Flower> Flowers { get; set;}
+        public DbSet<New> News { get; set; }
     }
 }
---
+++ FlowersShop/FlowersShop/Models/New.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FlowersShop.Models
+{
+    public class New
+    {
+        public Guid Id { get; set; }
+        public DateTime Date { get; set; }
+        public string SmallInfo { get; set; }
+        public string BigInfo { get; set; }
+    }
+}
